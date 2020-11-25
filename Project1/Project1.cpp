de49582145375969ccad681c8309b41842a71e25
Mateusz Galazka <mgmateuszgalazka860@gmail.com>
2020-11-25 19:10:12 +0100

    Version3

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -9,6 +9,9 @@ void setValue(int* a) {
     *a = 20;
 
 }
+void dodawanie(int a, int b,int &suma){
+    suma = (a+b);
+}
 
 
 int main()
@@ -18,8 +21,12 @@ int main()
     int c = 8;
     int d = 20;
     cout <<"Przed zmiana: "<< a << endl;
-    setValue(&a);
+    // setValue(&a);
     cout <<"Po zmianie: "<< a << endl;
+
+    int suma=0;
+    dodawanie(a,b,suma);
+    cout<<suma;
 
 }
 
