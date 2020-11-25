dbcbf3b678e6224a0c5c2a7bd4d6ad6b924af8f8
Mateusz Galazka <mgalazka@regenersis.local>
2020-11-25 18:01:57 +0100

    Version1

---
+++ Project1/Project1.cpp
@@ -0,0 +1,24 @@
+﻿// Project1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void setValue(int* a) {
+    *a = 20;
+
+}
+
+
+int main()
+{
+    int a = 2;
+    int b = 4;
+    int c = 8;
+    cout <<"Przed zmiana: "<< a << endl;
+    setValue(&a);
+    cout <<"Po zmianie: "<< a << endl;
+
+}
+
